708edaaff7628aa30d592ea62d78a97a42640aff
AmmarAyman108 <ammarayman3030810@gmail.com>
2024-08-20 18:41:09 +0300

    second update

--- android/app/src/main/kotlin/com/example/master_api/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/master_api/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.master_api
+git remote add origin https://github.com/AmmarAyman108/Master-API.gitpackage com.example.master_api
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/widgets/custom_input_field.dart
+++ lib/widgets/custom_input_field.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CustomInputField extends StatefulWidget {
-  final String labelText;
   final String hintText;
-  final bool suffixIcon;
-  final bool? isDense;
+  final Icon? suffixIcon;
   final bool obscureText;
   final TextEditingController? controller;
 
   const CustomInputField(
       {Key? key,
-      required this.labelText,
       required this.hintText,
-      this.suffixIcon = false,
-      this.isDense,
+      this.suffixIcon,
       this.obscureText = false,
       this.controller})
       : super(key: key);
@@ -27,52 +23,50 @@ class _CustomInputFieldState extends State<CustomInputField> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
+    return SizedBox(
       width: size.width * 0.9,
-      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 3),
-      child: Column(
-        children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              widget.labelText,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
+      child: TextFormField(
+        obscureText: (widget.obscureText && _obscureText),
+        decoration: InputDecoration(
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 17, horizontal: 20),
+          fillColor: const Color.fromARGB(255, 212, 212, 212),
+          filled: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(50),
+            borderSide: BorderSide.none,
           ),
-          TextFormField(
-            obscureText: (widget.obscureText && _obscureText),
-            decoration: InputDecoration(
-              isDense: (widget.isDense != null) ? widget.isDense : false,
-              hintText: widget.hintText,
-              suffixIcon: widget.suffixIcon
-                  ? IconButton(
-                      icon: Icon(
-                        _obscureText
-                            ? Icons.remove_red_eye
-                            : Icons.visibility_off_outlined,
-                        color: Colors.black54,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          _obscureText = !_obscureText;
-                        });
-                      },
-                    )
-                  : null,
-              suffixIconConstraints: (widget.isDense != null)
-                  ? const BoxConstraints(maxHeight: 33)
-                  : null,
-            ),
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            validator: (textValue) {
-              if (textValue == null || textValue.isEmpty) {
-                return 'required!';
-              }
-              return null;
-            },
-            controller: widget.controller,
-          ),
-        ],
+          hintText: widget.hintText,
+          suffixIcon: widget.obscureText
+              ? IconButton(
+                  icon: Icon(
+                    _obscureText
+                        ? Icons.remove_red_eye
+                        : Icons.visibility_off_outlined,
+                    color: Colors.black54,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                )
+              : Padding(
+                  padding: const EdgeInsetsDirectional.only(end: 15),
+                  child: widget.suffixIcon,
+                ),
+          // suffixIconConstraints: (widget.isDense != null)
+          //     ? const BoxConstraints(maxHeight: 33)
+          //     : null,
+        ),
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        validator: (textValue) {
+          if (textValue == null || textValue.isEmpty) {
+            return 'required!';
+          }
+          return null;
+        },
+        controller: widget.controller,
       ),
     );
   }
